53084b0a513c4254e7e0944d1c389d601d7e406f
quasar <gethsun09@gmail.com>
2024-05-11 01:18:10 +0300

    Updates Hemahope.sol

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -5,7 +5,7 @@ module.exports = {
   solidity: "0.8.0",
   networks: {
     rolx_100004_1: {
-      url: "https://rpc.rolxtwo.evm.ra.blumbus.noisnemyd.xyz",
+      url: "https://json-rpc.rolxtwo.evm.ra.blumbus.noisnemyd.xyz",
       chainId: 100004,
       accounts: [process.env.PRIVATE_KEY],
     },
